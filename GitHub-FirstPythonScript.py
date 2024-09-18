28c3a9fe154b1d26b340fab9254cc3f8017b1a69
Iphy16 <ifeomaokonye.france@gmail.com>
2024-09-18 11:40:30 -0500

    Update GitHub-FirstPythonScript.py
    Final Assignment

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-    File name: GitHub-FirstPythonScript.py
-    Author: ENTER YOUR  NAME
-    Description:  ENTER BRIEF DESCRIPTION
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT
+    File Name: River Length Calculation
+    Author: Ifeoma Okonye
+    Description: Calculate total stream length in specifed ecoregion
+    Date Created: 09/11/2024
     Python Version: 3.9.16
 """
 
@@ -14,3 +14,26 @@ import arcpy
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
+arcpy.env.workspace = "C:\\Users\\ifeom\\OneDrive - Kansas State University\Courses\\6. Fall 24\\GEOG728 - Python\\GitHub\\firstpythonscript-Iphy16\\GIS Project\\ExerciseData.gdb"
+
+#another way to import your workspace is to add an "r" in front of your directory and make the path a string by adding '', then your dont need to correct the backslash (\) to frontslash (/) or double backslash (\\).
+# arcpy.env.workspace = r'C:\Users\ifeom\OneDrive - Kansas State University\Courses\6. Fall 24\GEOG728 - Python\GitHub\firstpythonscript-Iphy16\GIS Project\ExerciseData.gdb'
+
+
+# Perform Geoprocessing
+selectEcoregion = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
+
+arcpy.analysis.Buffer('ks_ecoregions', 'ecoregionBuffer', '10 Kilometers', 'Full', 'ROUND', 'ALL')
+
+riversClip = arcpy.analysis.Clip('ks_major_rivers', 'ecoregionBuffer','RiverClip')
+
+# Add new field and populate attribute
+arcpy.management.AddField('RiverClip', 'Type', 'TEXT', 10,'','','Type', 'NULLABLE', 'NON_REQUIRED')
+
+arcpy.management.CalculateField('RiverClip', 'Type', "'River'", expression_type='PYTHON3')
+
+
+# Calculate Stream length
+## Set local variables
+
+sumStreamLen = sumStreamLen = arcpy.gapro.SummarizeAttributes('RiverClip', 'sumRiverLen', ['Type'], [['Shape_Length', 'SUM']])
