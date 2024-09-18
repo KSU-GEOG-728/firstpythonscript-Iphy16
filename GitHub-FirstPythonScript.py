aa4b36d3bd9661bb00cd1067bbb9d3a0132425be
Iphy16 <ifeomaokonye.france@gmail.com>
2024-09-18 12:31:39 -0500

    Update GitHub-FirstPythonScript.py
    Added Class Solutions

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -16,18 +16,14 @@ arcpy.env.overwriteOutput = True
 # Set current workspace
 arcpy.env.workspace = "C:\\Users\\ifeom\\OneDrive - Kansas State University\Courses\\6. Fall 24\\GEOG728 - Python\\GitHub\\firstpythonscript-Iphy16\\GIS Project\\ExerciseData.gdb"
 
-#another way to import your workspace is to add an "r" in front of your directory and make the path a string by adding '', then your dont need to correct the backslash (\) to frontslash (/) or double backslash (\\).
-# arcpy.env.workspace = r'C:\Users\ifeom\OneDrive - Kansas State University\Courses\6. Fall 24\GEOG728 - Python\GitHub\firstpythonscript-Iphy16\GIS Project\ExerciseData.gdb'
-
 
 # Perform Geoprocessing
 selectEcoregion = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
-# Option 2 if i dont want to create a new variable
-#arcpy.Select_analysis('ks_ecoregions', 'outSelect', "US_L3NAME = 'Flint Hills'")
 
-
+# Calculate Buffer
 arcpy.analysis.Buffer(selectEcoregion, 'ecoregionBuffer', '10 Kilometers', 'Full', 'ROUND', 'ALL')
 
+# Clip Features
 arcpy.analysis.Clip('ks_major_rivers', 'ecoregionBuffer','RiverClip')
 
 # Add new field and populate attribute
@@ -36,11 +32,34 @@ arcpy.management.AddGeometryAttributes('RiverClip', 'LENGTH', 'MILES_US')
 arcpy.management.AddField('RiverClip', 'Type', 'TEXT', 10,'','','Type', 'NULLABLE', 'NON_REQUIRED')
 arcpy.management.CalculateField('RiverClip', 'Type', "'River'", expression_type='PYTHON3')
 
-# Option 2 of doing this
-# arcpy.management.CalculateField('RiverClip', 'Length_Mi', '!Shape_Length!*0.000621371', "", "", "Float")
-# arcpy.Statistics_analysis('RiverClip', "outStats1", [["Length_Mi", "SUM"]])
-
-
 # Calculate Stream length
 arcpy.gapro.SummarizeAttributes('RiverClip', 'sumRiverLen', ['Type'], [['LENGTH', 'SUM']]) #when there are square brackets, its a list
 
+
+
+#################################################################################################################################
+# Method 2
+#################################################################################################################################
+
+# Import arcpy module and allow overwrites
+#import arcpy
+#arcpy.env.overwriteOutput = True
+
+# arcpy.env.workspace = r'C:\Users\ifeom\OneDrive - Kansas State University\Courses\6. Fall 24\GEOG728 - Python\GitHub\firstpythonscript-Iphy16\GIS Project\ExerciseData.gdb'
+
+
+## Option 2 if i dont want to create a new variable
+# Perform Geoprocessing
+#arcpy.Select_analysis('ks_ecoregions', 'outSelect', "US_L3NAME = 'Flint Hills'")
+
+# Calculate Buffer
+# arcpy.analysis.Buffer(selectEcoregion, 'ecoregionBuffer', '10 Kilometers', 'Full', 'ROUND', 'ALL')
+
+# Clip Features
+# arcpy.analysis.Clip('ks_major_rivers', 'ecoregionBuffer','RiverClip')
+
+# Calculate Length in Miles
+# arcpy.management.CalculateField('RiverClip', 'Length_Mi', '!Shape_Length!*0.000621371', "", "", "Float")
+
+# Summarize Length in Miles
+# arcpy.Statistics_analysis('RiverClip', "outStats1", [["Length_Mi", "SUM"]])
